e3db7dc1689d5dde0091dfe03adeb2b8b4b241aa
chotixity <melkychoti@gmail.com>
2024-06-03 20:03:38 +0300

    cicd

--- spoonacular/lib/main.dart
+++ spoonacular/lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:isolate';
-
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:spoonacular/models/recipe_store.dart';
 import 'package:spoonacular/presentation/screens/homepage.dart';
-import 'package:spoonacular/repository/api_service.dart';
 import 'package:spoonacular/repository/local_storage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
 
   // Hive.initFlutter();
